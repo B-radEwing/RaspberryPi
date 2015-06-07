2febde2bf04b3115b826906e19588807ef6b069a
Brad Ewing <bradleywewing@gmail.com>
2015-06-06 17:14:27 -0700

    Initial commit. Committing README and tweetIP.

---
+++ README.md
@@ -0,0 +1,6 @@
+## Raspberry Pi Scripts
+
+A collection of scripts I've written for my Raspberry Pi. 
+
+1. tweetIP.py
+* Tweets my local IP address. Usefull with an unpredictable IP on the local network.
---
+++ tweetIP.py
@@ -0,0 +1,23 @@
+import socket
+from twython import Twython
+
+def tweet(msg):
+        CONSUMER_KEY = "***"
+        CONSUMER_SECRET = "***"
+        ACCESS_KEY = "***"
+        ACCESS_SECRET = "***"
+
+        api = Twython(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET)
+        api.update_status(status=msg)
+
+def getIP():
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect("github.io", 80)
+        ip = s.getsockname()[0]
+        s.close()
+
+        return ip
+
+if __name__ == "__main__":
+        ip = getIP()
+        tweet(ip)
