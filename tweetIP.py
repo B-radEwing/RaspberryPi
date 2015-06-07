f15ef98ab52160ad2e0566bec9394aa4926ffffd
Brad Ewing <bradleywewing@gmail.com>
2015-06-06 17:30:50 -0700

    Added time and date to get around duplicate tweet error.

--- tweetIP.py
+++ tweetIP.py
@@ -1,23 +1,29 @@
-import socket
+import datetime, socket
 from twython import Twython
 
 def tweet(msg):
-        CONSUMER_KEY = "***"
-        CONSUMER_SECRET = "***"
-        ACCESS_KEY = "***"
-        ACCESS_SECRET = "***"
+        CONSUMER_KEY = "****"
+        CONSUMER_SECRET = "****"
+        ACCESS_KEY = "****"
+        ACCESS_SECRET = "****"
 
         api = Twython(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET)
         api.update_status(status=msg)
 
 def getIP():
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        s.connect("github.com", 80)
+        s.connect(("github.com", 80))
         ip = s.getsockname()[0]
         s.close()
 
         return ip
 
+def prepMSG(ip):
+        dt = datetime.datetime.now() # Import datetime to get around duplicate tweets
+        message = "Date & Time: " + str(dt) + " IP: " + ip
+        return message
+
 if __name__ == "__main__":
         ip = getIP()
-        tweet(ip)
+        msg = prepMSG(ip)
+        tweet(msg)
