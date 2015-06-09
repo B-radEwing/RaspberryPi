5cc56df62f18ddd7ce507d91fccb02e9dd462155
Brad Ewing <bradleywewing@gmail.com>
2015-06-08 19:31:55 -0700

    Adding temperature.sh and tweetPic.py

---
+++ tweetPic.py
@@ -0,0 +1,22 @@
+import datetime, socket, sys
+from twython import Twython
+
+def tweet(msg):
+        CONSUMER_KEY = ""****"
+        CONSUMER_SECRET = "****"
+        ACCESS_KEY = "****"
+        ACCESS_SECRET = "****"
+
+        photo = open(sys.argv[1], 'rb')
+
+        api = Twython(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET)
+        api.update_status_with_media(media=photo, status=msg)
+
+def prepMSG():
+        dt = datetime.datetime.now() # Import datetime to get around duplicate $
+        message = "Date & Time: " + str(dt)
+        return message
+
+if __name__ == "__main__":
+        msg = prepMSG()
+        tweet(msg)
